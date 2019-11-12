ddff83636fa3bc6d7411bbd244d38c8dc9805e3d
Glitch (thundering-sunfish) <none>
2019-11-12 17:35:07 +0000

    🎰🍽 Updated with Glitch

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
 
 var app = express();
+var upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +17,13 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile') ,function(req,res) {
+  if (req.file)
+    res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+  else
+    res.json({error: 'No file provided'})
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
